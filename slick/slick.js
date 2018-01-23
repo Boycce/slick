7dabacb09494bc4acbf8b5708152724d226de2cd
Paul Barrett <paulbarrett79@gmail.com>
2018-01-23 19:03:16 +0000

    infinite mode last to first targets wrong clone

--- slick/slick.js
+++ slick/slick.js
@@ -2321,7 +2321,7 @@
                 if (index === 0) {
 
                     allSlides
-                        .eq(allSlides.length - 1 - _.options.slidesToShow)
+                        .eq(0)
                         .addClass('slick-center');
 
                 } else if (index === _.slideCount - 1) {
